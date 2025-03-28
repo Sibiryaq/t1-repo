690075cb32e609c30f5d24afd6e38a7c9ee92815
Sibiryaq <4totakoeaaa@mail.ru>
2025-03-28 22:41:50 +0700

    @Repository хоть и так сработало бы

--- src/main/java/com/t1_academy/t1_repo/repository/TaskRepository.java
+++ src/main/java/com/t1_academy/t1_repo/repository/TaskRepository.java
@@ -1,6 +1,8 @@
 package com.t1_academy.t1_repo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
