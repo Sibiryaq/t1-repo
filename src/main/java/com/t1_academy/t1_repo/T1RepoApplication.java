66e02240f4bdde057e2a8ccb5a0cc6fde7cbe5b9
Sibiryaq <4totakoeaaa@mail.ru>
2025-03-28 12:59:08 +0700

    base template for deployment

---
+++ src/main/java/com/t1_academy/t1_repo/T1RepoApplication.java
@@ -0,0 +1,13 @@
+package com.t1_academy.t1_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1RepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1RepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t1_academy/t1_repo/T1RepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1_academy.t1_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1RepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
